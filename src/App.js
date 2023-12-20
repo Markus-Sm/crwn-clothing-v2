2ae004fa360754f9a380755e7588ae9bf82e951f
Marek Samofal <marek.samofal@gmail.com>
2023-12-20 11:15:56 +0100

    docs: update

--- src/App.js
+++ src/App.js
@@ -20,6 +20,9 @@ const App = () => {
         <div className='category-body-container'>
           <h2>Sneakers</h2>
           <p>Shop Now</p>
+          <p>
+            
+          </p>
         </div>
       </div>
       <div className='category-container'>
